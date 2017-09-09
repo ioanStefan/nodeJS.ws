bbdb9bc969136b9d05d013b29dd566fdcb801f15
ioanStefan <31771469+ioanStefan@users.noreply.github.com>
2017-09-09 12:40:01 +0300

    WebService

---
+++ src/web.js
@@ -0,0 +1,35 @@
+require('lodash');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var fs = require("fs");
+
+app.use(function (req, res, next) {
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.use(bodyParser.json());
+
+app.post('/getData', function (req, res) {
+    console.log(req.body);
+    res.end(JSON.stringify(req.body['name']));
+});
+var server = app.listen(8081, function () {
+    var host = server.address().address;
+    var port = server.address().port;
+    console.log("Example app listening at http://%s:%s", host, port);
+});
